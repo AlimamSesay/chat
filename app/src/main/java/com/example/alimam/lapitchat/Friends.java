ca9071bbc7f35c772e774ae23af65dccbea67cbb
Alimam <alimamsesay@ymail.com>
2017-11-07 14:29:24 +0200

    Complete the toturial

---
+++ app/src/main/java/com/example/alimam/lapitchat/Friends.java
@@ -0,0 +1,26 @@
+package com.example.alimam.lapitchat;
+
+/**
+ * Created by alimam on 11/7/17.
+ */
+
+public class Friends {
+
+    public String date;
+
+    public Friends(){
+
+    }
+
+    public Friends(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
